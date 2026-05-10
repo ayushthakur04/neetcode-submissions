30e25b62fe9eaabac691918094ef8ff1467e9911
Ayush Thakur <ayush20489@gmail.com>
2026-05-10 22:54:52 +0530

    Add: sort-an-array - submission-0

---
+++ Data Structures & Algorithms/sort-an-array/submission-0.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> sortArray(vector<int>& nums) {
+    for(int i=0;i<nums.size()-1;i++){
+        int minindex=i;
+
+        for(int j=i+1;j<nums.size();j++){
+
+            if(nums[j]<nums[minindex])
+            minindex=j;
+        }
+        swap(nums[minindex],nums[i]);
+    }
+    return nums;
+    }
+};
